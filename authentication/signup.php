3d4706560d04eeaf61276c8d5c65b7b512746577
osama <osama@osama.osama>
2025-05-24 14:03:17 +0300

    adding my project files

---
+++ authentication/signup.php
@@ -0,0 +1,39 @@
+<?php
+
+
+// echo 'hello from sign up';
+
+// this is a comment
+
+
+/*
+this is a comment
+this is
+*/
+
+// if() {
+
+// } else {
+
+// }
+
+
+
+
+// $name = 'osama';
+// $int = 20;
+// $float = 20.9;
+// $bool = true;
+
+// $first name = 'osama';
+
+// echo var_dump($name);
+// echo var_dump($bool);
+
+echo 'osama';
+echo "<br>";
+echo 'hello world';
+// firstNameAge
+
+// echo $name . ' ?' . $lastName;
+// echo 'the user name is ' . $name . ' and the age is ' . $age;
